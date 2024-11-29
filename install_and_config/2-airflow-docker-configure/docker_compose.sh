7a8880410d0fd3027eab60740a5eeaee05d7672e
= <=>
2024-11-29 19:11:45 +0530

    updated doc

--- install_and_config/2-airflow-docker-configure/docker_compose.sh
+++ install_and_config/2-airflow-docker-configure/docker_compose.sh
@@ -6,11 +6,12 @@
 """ Follow following Steps to setup Docker container for Airflow
 # 1. Download docker-compose.yaml file from the above URL
 # 2. If Windows OS is being used then replace volume Environmental varialble (${AIRFLOW_PROJ_DIR:-.}) to windows folder. (for ref. check the docker compose file present here).
+# 2.1. windows path is added as mnt and escape character (\) for spaces: "/mnt/c/Users/jerin/OneDrive\ -\ Aligned Automation/Work/Learn/Tech Solutions/airflow-learn-hands-on/main/airflow-working-folder/dags:/opt/airflow/dags"  
 # 3. Download Docker Desktop for Windows and run it
 # 4. Execute the below command to pull and configure all the images defined in the docker-compose.yaml file
 # these commands need to be executed in the same folder where docker-compose.yaml file exists
 
-Step 2 is required to mount host system folder to Airflow Docker, this will help to move files between Host and Docker 
+Step 2 in above points, is required to mount host system folder to Airflow Docker, this will help to move files between Host and Docker 
 """
 
 docker compose up -d # to start all the services mentioned in the docker compose yaml file
